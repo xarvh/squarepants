683fa0e12106a065a3d83ee494b178f07b146bda
xarvh <francesco.orsenigo@gmail.com>
2020-10-20 21:06:32 +0200

    wip

---
+++ src/something.elm
@@ -0,0 +1,146 @@
+module Main exposing (..)
+
+
+type OpenOrClose = Open | Close
+
+
+type Token
+  = Word String
+  | Operator String
+  | Comma
+  | RoundParen OpenOrClose
+  | SquareBracket OpenOrClose
+  | CurlyBrace OpenOrClose
+  | StringLiteral String
+  | NumberLiteral String
+
+
+{-| TODO should also contain info about comments and token positions?
+-}
+type Line = Line (List Token) (List Line)
+
+
+
+-------
+
+
+words = /[a-zA-Z._][a-zA-Z._0-9]*/
+parens = /[,()[]{}]/
+ops = /[=+-*/:><!^|]/
+
+
+
+function chunkToTokens(chunkAsString) {
+
+  let s = chunkAsString.trimStart();
+
+  s.match();
+
+
+
+
+}
+
+
+
+chunkToTokens c accum =
+  Regex.split () c
+  String.trimLeft
+    |> 
+
+
+
+
+
+
+
+
+
+
+type Consuming =
+    Nothing
+    | Word String
+    | Operators String
+
+
+
+
+
+charToStuff c =
+
+      if isWord c then
+        let
+            (state, prev) =
+                case consuming of
+                  Word s ->
+                    (state, s)
+                  _ ->
+                    (closePreviousToken state, "")
+        in
+              { state | consuming = Word c }
+
+      else if isOperator c then
+        { state | consuming = Operator c }
+      else if isParen c then
+        { state | tokens = Paren c :: state.tokens }
+      else
+        state
+
+
+
+
+    Just (Word s) ->
+      if isWord c then
+        { state | consuming = Word <| c :: s }
+
+
+
+      else if isOperator c then
+        { state | consuming = Operator c }
+      else if isParen c then
+        { state | tokens = Paren c :: state.tokens }
+      else
+        state
+
+
+    
+
+
+
+
+
+
+
+getParent : Node -> Maybe Node
+getParent n =
+    Debug.todo
+
+
+type alias State =
+    { root : Node
+    , last : Node
+    }
+
+
+resolveNesting : String -> State -> State
+resolveNesting s state =
+    case s of
+        "if" ->
+            case getParent state.last of
+
+            xxx
+
+        "then" ->
+            aaaa
+
+        "else" ->
+            xxxx
+
+        "(" ->
+            xxxx
+
+        ")" ->
+            hhhh
+
+        _ ->
+          ????
--- src/tokenizeChunk.js
+++ src/tokenizeChunk.js
@@ -1,4 +1,65 @@
 
+
+
+
+
+
+
+
+
+function ContentLineToTokens(chunkAsString) {
+
+  const trimmed = chunkAsString.trimStart();
+  if (!trimmed) return
+
+  let types = [{
+    regex: /^[a-zA-Z._][a-zA-Z._0-9]*/,
+    id: 'Word',
+  }, {
+    regex: /^[,()\[\]{}]/,
+    id: 'Parens',
+  }, {
+    regex: /^[=+\-*/:><!^|]/,
+    id: 'Operator',
+  }];
+
+
+  const re = () => {
+    const type = types.pop();
+    if (!type) throw new Error('unrecognised token "' + trimmed + '"');
+
+    const match = trimmed.match(type.regex);
+    if (!match) return re();
+
+    return {
+      content: match[0],
+      type: type.id,
+      rest: trimmed.slice(match[0].length),
+    };
+  }
+
+  return re();
+}
+
+
+
+
+
+console.log(ContentLineToTokens(' asdf + - sdf.asasd_asdf'))
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
 //Se riduco a token, poi posso scasinare con il formato come cazzo mi pare e rendere piú facile ?
 //tokenisation is mostly to PRODUCE ERROR MESSAGES
 //break down control structures, whatever is left are expressions/commands?
@@ -32,15 +93,35 @@ function makeInitialState() {
   };
 
   return {
+    position: 0,
     root,
     lastAdded: root,
   }
 }
 
 
+
 function resolveNesting(state, chunk) {
 
+  switch (toString(chunk)) {
+    case "if":
+      add another node, no problem
 
+    case "then":
+      assert: parent MUST be an "if"
+      open a "then" child for the "if" node
+
+    case "else":
+      assert parent MUST be an "if" AND MUST have a "then" already populated
+
+    case "indentation up":
+      if parent is an "if/then": go out of the "if/then"
+
+
+
+
+
+  }
 
 }
 
@@ -52,4 +133,4 @@ function resolveNesting(state, chunk) {
 
 
 
-
+*/
